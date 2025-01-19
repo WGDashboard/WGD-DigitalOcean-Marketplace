b229d9dcf6e0249817454e4b1449b97fbf0bbf92
Donald Zou <donaldzou@live.hk>
2025-01-19 18:04:40 +0800

    Update 01-firstrun.sh

--- files/var/lib/cloud/scripts/per-instance/01-firstrun.sh
+++ files/var/lib/cloud/scripts/per-instance/01-firstrun.sh
@@ -8,6 +8,7 @@ privateKey=$(wg genkey)
 cat > /etc/wireguard/wg0.conf << EOL
 [Interface]
 PrivateKey = ${privateKey}
+Address = 10.0.0.0/24
 SaveConfig = true
 PostUp = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -A FORWARD -o wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE;
 PreDown = 
