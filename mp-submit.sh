611c8b6df04ab99d14ffd548db1107c9e57f11c8
Donald Zou <donaldzou@live.hk>
2025-05-17 16:08:31 +0800

    Update mp-submit.sh

--- mp-submit.sh
+++ mp-submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 IMG_ID=$(cat manifest.json | jq .builds[-1].artifact_id | tr -d '"' | cut -d':' -f2)
-APP_VERSION=$($venv_python -c "import json; import urllib.request; data = urllib.request.urlopen('https://api.github.com/repos/donaldzou/WGDashboard/releases/latest').read(); output = json.loads(data);print(output['tag_name'])")
+APP_VERSION=$(python3 -c "import json; import urllib.request; data = urllib.request.urlopen('https://api.github.com/repos/donaldzou/WGDashboard/releases/latest').read(); output = json.loads(data);print(output['tag_name'])")
 
 echo "$IMG_ID"
 echo "$APP_VERSION"
