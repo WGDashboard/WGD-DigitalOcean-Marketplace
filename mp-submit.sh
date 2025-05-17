507c01f24d2edbe8863d6e6d1c3c7cc4bbcf0220
Donald Zou <donaldzou@live.hk>
2025-05-17 16:07:44 +0800

    Update mp-submit.sh

--- mp-submit.sh
+++ mp-submit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-IMG_ID=$(jq '.builds[-1].artifact_id | split(":")[1] | tonumber' manifest.json)
+IMG_ID=$(cat manifest.json | jq .builds[-1].artifact_id | tr -d '"' | cut -d':' -f2)
 APP_VERSION=$($venv_python -c "import json; import urllib.request; data = urllib.request.urlopen('https://api.github.com/repos/donaldzou/WGDashboard/releases/latest').read(); output = json.loads(data);print(output['tag_name'])")
 
-echo $IMG_ID
-echo $APP_VERSION
+echo "$IMG_ID"
+echo "$APP_VERSION"
 
 # curl -X PATCH -H "Content-Type: application/json" -H "Authorization: Bearer ${DIGITALOCEAN_API_TOKEN}" -d "{\"reasonForUpdate\": \"new version\", \"version\": \"${APP_VERSION}\", \"imageId\": ${IMG_ID}}" https://api.digitalocean.com/api/v1/vendor-portal/apps/${APP_ID}
